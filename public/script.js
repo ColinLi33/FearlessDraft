c0e99fc26c79e80c63134d5d964b2f240d5c5225
ColinLi33 <Colin8tor1206@gmail.com>
2024-09-08 13:44:57 -0700

    started work on basic draft stuff

--- public/script.js
+++ public/script.js
@@ -2,90 +2,144 @@ const socket = io();
 const patch = '14.17.1'
 const baseUrl = `http://ddragon.leagueoflegends.com/cdn/${patch}`
 let champions = null;
+let currentPick = 1
+const preloadedImages = {};
 
-fetch(`${baseUrl}/data/en_US/champion.json`)
-    .then(response => response.json())
-    .then(data => {
-        champions = data.data
-        displayChampions(champions);
-    })
-    .catch(error => {
-        console.error('Error fetching champion data:', error);
-});
-
-const championGrid = document.getElementById('champion-grid');
-let selectedChampion = null;
-function displayChampions(champions) {
-    championGrid.innerHTML = '';
+function preloadChampionImages() {
     Object.keys(champions).forEach(championKey => {
         const champion = champions[championKey];
-        const championIcon = document.createElement('img');
-        championIcon.src = `${baseUrl}/img/champion/${champion.id}.png`;
-        championIcon.alt = champion.id;
-        championIcon.classList.add('champion-icon');
-        championIcon.addEventListener('click', () => {
-            if (selectedChampion) {
-                selectedChampion.classList.remove('selected');
-            }
-            selectedChampion = championIcon;
-            selectedChampion.classList.add('selected');
-            confirmButton.disabled = false;
-        });
-        championGrid.appendChild(championIcon);
+        const championImage = new Image();
+        if(champion.id === 'Fiddlesticks') { //LOL!
+            championImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/centered/FiddleSticks_0.jpg`;
+        } else {
+            championImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/centered/${champion.id}_0.jpg`;
+        }
+        preloadedImages[champion.id] = championImage;
     });
 }
 
+fetch(`${baseUrl}/data/en_US/champion.json`)
+	.then(response => response.json())
+	.then(data => {
+		champions = data.data;
+        champions = Object.entries(champions).map(([key, value]) => ({
+            id: value.id,
+            key: value.key,
+        }));
+        preloadChampionImages();
+		return fetch('/proxy/championrates');
+	})
+	.then(response => response.json())
+	.then(data => {
+		roleData = data;
+		mergeRoleData(roleData.data);
+		displayChampions(champions);
+	})
+	.catch(error => {
+		console.error('Error fetching data:', error);
+	});
 
+function mergeRoleData(roleData) {
+	Object.keys(champions).forEach(champ => {
+		const key = champions[champ].key.toString();
+		if (roleData[key]) {
+			const roles = roleData[key];
+			roleTest = Object.entries(roles).map(([role, data]) => ({
+				role: role,
+				playRate: data.playRate
+			}))
+			roleTest = roleTest.filter(role => role.playRate > 0);
+			roleTest = roleTest.map(role => role.role);
+			champions[champ].roles = roleTest;
+		} else {
+			champ.roles = [];
+		}
+	});
+}
+
+const championGrid = document.getElementById('champion-grid');
+let selectedChampion = null;
+
+function displayChampions(champions) {
+	championGrid.innerHTML = '';
+	Object.keys(champions).forEach(championKey => {
+		const champion = champions[championKey];
+		const championIcon = document.createElement('img');
+		championIcon.src = `${baseUrl}/img/champion/${champion.id}.png`;
+		championIcon.alt = champion.id;
+		championIcon.classList.add('champion-icon');
+		championIcon.addEventListener('click', () => {
+            const pickSlot = document.querySelector(`#blue-picks .pick-slot:nth-child(${currentPick})`);
+            const pickImage = pickSlot.querySelector('img');
+			if (selectedChampion === championIcon) {
+                pickImage.src = '/img/placeholder.png';
+                selectedChampion = null;
+                confirmButton.disabled = true;
+			} else {
+                pickImage.src = preloadedImages[champion.id].src;
+                selectedChampion = championIcon;
+                confirmButton.disabled = false;
+            }
+		});
+		championGrid.appendChild(championIcon);
+	});
+}
 
 
 function filterChampions() {
-    const searchTerm = searchInput.value.toLowerCase();
-    const filteredChampions = Object.values(champions).filter(champion => {
-        const matchesRole = selectedRole === '' || champion.roles[0] == selectedRole;
-        const matchesSearch = champion.name.toLowerCase().includes(searchTerm);
-        return matchesRole && matchesSearch;
-    });
+	const searchTerm = searchInput.value.toLowerCase();
+	const filteredChampions = Object.values(champions).filter(champion => {
+		const matchesRole = selectedRole === '' || champion.roles.includes(selectedRole.toUpperCase());
+		const matchesSearch = champion.id.toLowerCase().includes(searchTerm);
+		return matchesRole && matchesSearch;
+	});
 
-    displayChampions(filteredChampions);
+	displayChampions(filteredChampions);
 }
 
 const roleIcons = document.querySelectorAll('.role-icon');
 let selectedRole = '';
 roleIcons.forEach(icon => {
-    icon.addEventListener('click', () => {
-      const role = icon.getAttribute('data-role');
-      if (selectedRole === role) {
-        selectedRole = '';
-        icon.classList.remove('active');
-      } else {
-        selectedRole = role;
-        roleIcons.forEach(icon => icon.classList.remove('active'));
-        icon.classList.add('active');
-      }
-      filterChampions();
-    });
-  });
+	icon.addEventListener('click', () => {
+		const role = icon.getAttribute('data-role');
+		if (selectedRole === role) {
+			selectedRole = '';
+			icon.classList.remove('active');
+		} else {
+			selectedRole = role;
+			roleIcons.forEach(icon => icon.classList.remove('active'));
+			icon.classList.add('active');
+		}
+		filterChampions();
+	});
+});
 
 const searchInput = document.getElementById('searchInput');
 searchInput.addEventListener('input', filterChampions);
 
 const confirmButton = document.getElementById('confirmButton');
 confirmButton.addEventListener('click', () => {
-    if (selectedChampion) {
-        const championName = selectedChampion.alt;
-        const champion = champions.find(champ => champ.name === championName);
+	if (selectedChampion) {
+		const championName = selectedChampion.alt;
+        const champion = Object.values(champions).find(champ => champ.id === championName);
         handleDraftSelection(champion);
-        selectedChampion.classList.remove('selected');
-        selectedChampion = null;
-        confirmButton.disabled = true;
-    }
+		selectedChampion.classList.remove('selected');
+		selectedChampion = null;
+		confirmButton.disabled = true;
+	}
 });
 
 function handleDraftSelection(champion) {
-    socket.emit('draftSelection', champion);
+	socket.emit('draftSelection', champion);
+    const pickSlot = document.querySelector(`#blue-picks .pick-slot:nth-child(${currentPick})`);
+    const championImage = document.createElement('img');
+    championImage.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/centered/${champion.id}_0.jpg`;
+    pickSlot.innerHTML = '';
+    pickSlot.appendChild(championImage);
+    currentPick++;
 }
 
 
 socket.on('draftUpdate', (data) => {
-    console.log('Draft update:', data);
+	console.log('Draft update:', data);
 });
--- server.js
+++ server.js
@@ -9,21 +9,33 @@ const io = socketIO(server);
 
 app.use(express.static('public'));
 
+app.get('/proxy/championrates', async (req, res) => { //TODO: cache later
+	try {
+		const response = await fetch('https://cdn.merakianalytics.com/riot/lol/resources/latest/en-US/championrates.json');
+		const data = await response.json();
+		res.json(data);
+	} catch (error) {
+		console.error('Error fetching data:', error);
+		res.status(500).json({
+			error: 'Failed to fetch data'
+		});
+	}
+});
+
 io.on('connection', (socket) => {
-    console.log('A user connected');
+	console.log('A user connected');
 
+	socket.on('draftSelection', (data) => {
+		console.log('Draft selection:', data);
+		io.emit('draftUpdate', data);
+	});
 
-    socket.on('draftSelection', (data) => {
-        console.log('Draft selection:', data);
-        io.emit('draftUpdate', data);
-    });
-
-    socket.on('disconnect', () => {
-        console.log('A user disconnected');
-    });
+	socket.on('disconnect', () => {
+		console.log('A user disconnected');
+	});
 });
 
 const port = process.env.PORT || 3333;
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
